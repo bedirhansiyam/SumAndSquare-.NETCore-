264a9ba5cc5a06205f7735ea09d5408277abcdff
Bedirhan <bedirhansiyam@gmail.com>
2023-03-21 17:27:12 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace SumAndSquare;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isInt = true;
+        List<int> numbers = new List<int>();
+        int sumOfLess = 0; 
+        double sumofGreaterSquare = 0;
+
+        Console.Write("Please enter space-separated integers : ");
+
+        do
+        {
+            string[] strNumbers = Console.ReadLine().Split(' ');
+  
+            for (int i = 0; i < strNumbers.Count(); i++)
+            {
+                isInt = int.TryParse(strNumbers[i], out int number);
+                if(isInt == false)
+                {
+                    Console.WriteLine("");
+                    Console.Write("Please enter only integers : ");
+                    numbers.Clear();
+                    break;
+                }
+                else
+                    numbers.Add(number);
+            }            
+        } while (isInt == false);
+
+        foreach (var number in numbers)
+        {
+            if(number < 67)
+            {
+                int diff = 67 - number;
+                sumOfLess += diff;
+            }
+            else if(number > 67)
+            {
+                double diff = number - 67;
+                sumofGreaterSquare += Math.Pow(diff,2);
+            }
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Result : " + sumOfLess + " " + sumofGreaterSquare);
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Sum And Sum Of Squares - .Net Core
+In this application, the user enters *n* space-separated integers and the application checks if these numbers are greater or less than *67*. The application calculates the sum of the differences of the smaller ones and the sum of the squares of the differences of the bigger ones and prints it to the console.
+</br>
+
+*Sample input: 56 45 68 77* </br>
+*Sample output: 33 101*
+
+</br>
+
+[Patika.dev](https://app.patika.dev/)
